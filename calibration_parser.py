2ea4a1a745aeb5426b6715c8fb1b8e67fbeabd04
Ned <ned143103@gmail.com>
2024-10-21 22:03:45 +0800

    Append .gitignore and fix argparse size problem.

--- calibration_parser.py
+++ calibration_parser.py
@@ -4,7 +4,7 @@ def calibration_parser():
     # args
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-s", "--size", help="Enter number of row/col points ex:6 7", type=array, default=[6, 7])
+        "-s", "--size", help="Enter number of row/col points ex:6 7", nargs = 2, type=int, default=[6, 8])
     parser.add_argument(
         "-m", "--mm", help="enter square length of chessboard defaute is 1", type=int, default=1)
     parser.add_argument(
